217706cc9756b36ce094a845d59c2110cdafeabb
TonyTrem <117864473+TonyTrem@users.noreply.github.com>
2023-02-15 23:17:27 +1300

    Complete leapYear exercise

--- javascript/javascript-exercises-main/06_leapYears/leapYears.js
+++ javascript/javascript-exercises-main/06_leapYears/leapYears.js
@@ -1,5 +1,19 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if (!Number.isInteger(year)) {
+        return "ERROR";
+    }
 
+    return year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0) ? true : false;
+    
+    // if (year % 400 === 0) {
+    //     return true;
+    // } else if (year % 100 === 0) {
+    //     return false;
+    // } else if (year % 4 === 0) {
+    //     return true;
+    // } else {
+    //     return false;
+    // }
 };
 
 // Do not edit below this line
