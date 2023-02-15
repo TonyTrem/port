58f7b7211b57f4ab8d83a4b3a8af88f77e1c0988
TonyTrem <117864473+TonyTrem@users.noreply.github.com>
2023-02-15 22:17:14 +1300

    Complete removeFromArray exercise

--- javascript/javascript-exercises-main/04_removeFromArray/removeFromArray.js
+++ javascript/javascript-exercises-main/04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
 const removeFromArray = function() {
-
+    const myArray = arguments[0];
+    for (let i = 1; i < arguments.length; i++) {  
+        const item = arguments[i];
+        if (myArray.includes(item)) {
+            const index = myArray.indexOf(item);
+            myArray.splice(index, 1);  
+        }     
+               
+    }    
+    return myArray;
 };
 
 // Do not edit below this line
