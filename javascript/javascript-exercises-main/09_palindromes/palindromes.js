b71a4d757732e9c336cd9310159d566879d5f9ca
TonyTrem <117864473+TonyTrem@users.noreply.github.com>
2023-02-16 01:05:40 +1300

    Complete palindromes exercise

--- javascript/javascript-exercises-main/09_palindromes/palindromes.js
+++ javascript/javascript-exercises-main/09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    // Remove punctuation and make it all lower case
+    let checkString = str.replace(/[\p{P}$+<=>^`|~]/gu, '').toLowerCase();
 
+    // Remove all the whitespace
+    checkString = checkString.replace(/\s+/g, '');
+
+    // iterate over both halves of the string comparing characters
+    for (let i = 0; i < checkString.length / 2; i++)
+        if (checkString[i] === checkString[(checkString.length - 1) - i]) {
+            continue;
+        } else {
+            return false;
+        }
+    return true;
 };
 
 // Do not edit below this line
