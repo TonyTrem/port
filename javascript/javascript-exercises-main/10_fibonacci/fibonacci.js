a4984b4d05e5725f075c77248450d74bed8bedd7
TonyTrem <117864473+TonyTrem@users.noreply.github.com>
2023-02-16 01:35:18 +1300

    Complete fibonacci exercise

--- javascript/javascript-exercises-main/10_fibonacci/fibonacci.js
+++ javascript/javascript-exercises-main/10_fibonacci/fibonacci.js
@@ -1,6 +1,22 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function (n) {
+  if (n <= 0) {
+    return 'OOPS'
+  } else if (n === 1 || n === 2) {
+    return 1
+  } else {
+    let current = 1
+    let previous = 1
+    let next
+    let count = 2
+    while (count < n) {
+      next = current + previous
+      previous = current
+      current = next
+      count++
+    }
+    return current
+  }
+}
 
 // Do not edit below this line
-module.exports = fibonacci;
+module.exports = fibonacci
