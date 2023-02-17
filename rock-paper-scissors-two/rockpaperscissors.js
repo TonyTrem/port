270c4189d04acd6dcec2091b916f6d1450f3f14e
TonyTrem <117864473+TonyTrem@users.noreply.github.com>
2023-02-17 23:38:12 +1300

    Make the game run in a a class

--- rock-paper-scissors-two/rockpaperscissors.js
+++ rock-paper-scissors-two/rockpaperscissors.js
@@ -1,86 +1,91 @@
-const playButton = document.getElementById('playbtn');
-const startContainer = document.getElementById('start-container');
-const gameContainer = document.getElementById('container');
-const choices = ["rock", "paper", "scissors"];      
+class Game {
+  constructor() {
+    this.startButton = document.querySelector('#playbtn');
+    this.startContainer = document.querySelector('#start-container');
+    this.gameContainer = document.querySelector('#container');
+    this.selectionButtons = document.querySelectorAll('.selectionbtn');
+    this.playerScoreDisplay = document.querySelector('#player-score');
+    this.computerScoreDisplay = document.querySelector('#computer-score');
+    this.resultContainer = document.querySelector('#result-container');
+    this.choices = ["rock", "paper", "scissors"];
+    this.winConditions = {
+      'rock': 'scissors',
+      'paper': 'rock',
+      'scissors': 'paper'
+    };
+    this.playerScore = 0;
+    this.computerScore = 0;
+  }
 
-const buttons = document.querySelectorAll('.selectionbtn');
-const playerScoreDisplay = document.querySelector('#player-score');
-const computerScoreDisplay = document.querySelector('#computer-score');
-const resultContainer = document.querySelector('#result-container');
-
-let playerScore = 0;
-let computerScore = 0;
-
-playButton.addEventListener('click', () => {
-    startContainer.style.display = 'none';
-    gameContainer.style.display = 'flex';
-});        
-
-buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-        const result = playRound(button.id, getComputerChoice());
-        showResult(result);
-        updateScores(result);
+  start() {
+    this.startButton.addEventListener('click', () => {
+      this.startContainer.style.display = 'none';
+      this.gameContainer.style.display = 'flex';
     });
-});
-        
-function showResult(str) {
-    // Remove all child elements from the resultContainer
-    while (resultContainer.firstChild) {
-        resultContainer.removeChild(resultContainer.firstChild);
+
+    this.selectionButtons.forEach((button) => {
+      button.addEventListener('click', () => {
+        const result = this.playRound(button.id, this.getComputerChoice());
+        this.showResult(result);
+        this.updateScores(result);
+      });
+    });
+  }
+
+  playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+      return "Draw!";
+    } else if (this.winConditions[playerSelection] === computerSelection) {
+      return "You Won!";
+    } else {
+      return "You Lose!";
+    }
+  }
+
+  getComputerChoice() {
+    return this.choices[random(0, this.choices.length)];
+  }
+
+  showResult(str) {
+    while (this.resultContainer.firstChild) {
+      this.resultContainer.removeChild(this.resultContainer.firstChild);
     }
 
     const div = document.createElement('div');
     div.classList.add('result');
     div.textContent = str;
 
-    resultContainer.appendChild(div);    
+    this.resultContainer.appendChild(div);
+  }
+
+  updateScores(result) {
+    if (result === "You Won!") {
+      this.playerScore++;
+      this.playerScoreDisplay.textContent = this.playerScore;
+    } else if (result === "You Lose!") {
+      this.computerScore++;
+      this.computerScoreDisplay.textContent = this.computerScore;
+    }
+
+    if (this.playerScore === 5) {
+      this.showResult("You won the game!");
+      this.disableButtons();
+    } else if (this.computerScore === 5) {
+      this.showResult("You lost the game!");
+      this.disableButtons();
+    }
+  }
+
+  disableButtons() {
+    this.selectionButtons.forEach((button) => {
+      button.disabled = true;
+    });
+  }
 }
 
 function random(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
+  return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function getComputerChoice() {
-    return choices[random(0, choices.length)];
-}
-
-function playRound(playerSelection, computerSelection) {
-    const winConditions = {
-        'rock': 'scissors',
-        'paper': 'rock',
-        'scissors': 'paper'
-    };
-
-    if (playerSelection === computerSelection) {
-        return "Draw!";
-    } else if (winConditions[playerSelection] === computerSelection) {
-        return "You Won!";
-    } else {
-        return "You Lose!";
-    }
-}
-
-function updateScores(result) {
-    if (result === "You Won!") {
-        playerScore++;
-        playerScoreDisplay.textContent = playerScore;
-    } else if (result === "You Lose!") {
-        computerScore++;
-        computerScoreDisplay.textContent = computerScore;
-    }
-
-    if (playerScore === 5) {
-        showResult("You won the game!");
-        disableButtons();
-    } else if (computerScore === 5) {
-        showResult("You lost the game!");
-        disableButtons();
-    }
-}
-
-function disableButtons() {
-    buttons.forEach((button) => {
-        button.disabled = true;
-    });
-}
+const game = new Game();
+game.start();
