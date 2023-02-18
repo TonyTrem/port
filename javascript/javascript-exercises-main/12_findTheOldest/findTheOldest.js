992dd271647d41664740bee80c080ef725c4e818
TonyTrem <117864473+TonyTrem@users.noreply.github.com>
2023-02-18 19:05:32 +1300

    Complete findTheOldest

--- javascript/javascript-exercises-main/12_findTheOldest/findTheOldest.js
+++ javascript/javascript-exercises-main/12_findTheOldest/findTheOldest.js
@@ -1,6 +1,34 @@
 const findTheOldest = function() {
-
+    let oldest = 0;
+    let oldestIndex = 0;
+    let currentAge = 0;
+    let currentDate = new Date();
+    let currentYear = currentDate.getFullYear();
+    let yearOfBirth = 0;
+    let yearOfDeath = 0;
+    let people = arguments[0];
+    if (people === undefined) {
+        return undefined;
+    }
+    for (let i = 0; i < people.length; i++) {
+        if (people[i].yearOfBirth === undefined) {
+            return undefined;
+        }
+        if (people[i].yearOfDeath === undefined) {
+            yearOfDeath = currentYear;
+        } else {
+            yearOfDeath = people[i].yearOfDeath;
+        }
+        yearOfBirth = people[i].yearOfBirth;
+        currentAge = yearOfDeath - yearOfBirth;
+        if (currentAge > oldest) {
+            oldest = currentAge;
+            oldestIndex = i;
+        }
+    }
+    return people[oldestIndex];
 };
+    
 
 // Do not edit below this line
 module.exports = findTheOldest;
