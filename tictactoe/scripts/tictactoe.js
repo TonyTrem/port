5282b8e0caf966e71cd16b105a19fb54cffb9de9
TonyTrem <117864473+TonyTrem@users.noreply.github.com>
2023-02-26 05:39:03 +1300

    Add minimax algorithm for optimal computer play

--- tictactoe/scripts/tictactoe.js
+++ tictactoe/scripts/tictactoe.js
@@ -40,49 +40,94 @@ function playerMove(card) {
     gameBoard[id] = "X";
     card.innerHTML = "X";
     checkWin();
-
-    console.log("notifications: " + document.getElementById("notifications").innerHTML);
     if (document.getElementById("notifications").innerHTML === "") {
-        console.log("calling computerMove()");
         computerMove();
     }
 }
 
 function computerMove() {
-    for (let i = 0; i < winConditions.length; i++) {
-        let winCondition = winConditions[i];
-        let a = gameBoard[winCondition[0]];
-        let b = gameBoard[winCondition[1]];
-        let c = gameBoard[winCondition[2]];
-
-        if (a === "X" && b === "X" && c === "-") {
-            gameBoard[winCondition[2]] = "O";
-            document.getElementById(winCondition[2]).innerHTML = "O";
-            checkWin();
-            return;
+    let bestScore = -Infinity;
+    let move;
+  
+    for (let i = 0; i < gameBoard.length; i++) {
+        if (gameBoard[i] === "-") {
+            gameBoard[i] = "O";
+            let score = minimax(gameBoard, 0, false);
+            gameBoard[i] = "-";
+            if (score > bestScore) {
+                bestScore = score;
+                move = i;
+            }
         }
-        if (a === "X" && b === "-" && c === "X") {
-            gameBoard[winCondition[1]] = "O";
-            document.getElementById(winCondition[1]).innerHTML = "O";
-            checkWin();
-            return;
-        }
-        if (a === "-" && b === "X" && c === "X") {
-            gameBoard[winCondition[0]] = "O";
-            document.getElementById(winCondition[0]).innerHTML = "O";
-            checkWin();
-            return;
-        }
-
     }
-    let randomMove = Math.floor(Math.random() * 9);
-    while (gameBoard[randomMove] !== "-") {
-        randomMove = Math.floor(Math.random() * 9);
-    }
-    gameBoard[randomMove] = "O";
-    document.getElementById(randomMove).innerHTML = "O";
+  
+    gameBoard[move] = "O";
+    document.getElementById(move).innerHTML = "O";
     checkWin();
 }
+  
+function minimax(board, depth, isMaximizingPlayer) {
+    let result = checkGameState(board);
+  
+    if (result !== null) {
+        return result;
+    }
+  
+    if (isMaximizingPlayer) {
+        let bestScore = -Infinity;
+        for (let i = 0; i < board.length; i++) {
+            if (board[i] === "-") {
+                board[i] = "O";
+                let score = minimax(board, depth + 1, false);
+                board[i] = "-";
+                bestScore = Math.max(bestScore, score);
+            }
+        }
+        return bestScore;
+    } else {
+        let bestScore = Infinity;
+        for (let i = 0; i < board.length; i++) {
+            if (board[i] === "-") {
+                board[i] = "X";
+                let score = minimax(board, depth + 1, true);
+                board[i] = "-";
+                bestScore = Math.min(bestScore, score);
+            }
+        }
+        return bestScore;
+    }
+}
+  
+function checkGameState(board) {
+        for (let i = 0; i < winConditions.length; i++) {
+        let winCondition = winConditions[i];
+        let a = board[winCondition[0]];
+        let b = board[winCondition[1]];
+        let c = board[winCondition[2]];
+        if (a === "-" || b === "-" || c === "-") {
+            continue;
+        }
+        if (a === b && b === c) {
+            if (a === "X") {
+                return -1;
+            } else {
+                return 1;
+            }
+        }
+    }
+        let isDraw = true;
+        for (let i = 0; i < board.length; i++) {
+            if (board[i] === "-") {
+                isDraw = false;
+                break;
+            }
+        }
+        if (isDraw) {
+            return 0;
+        }
+            return null;
+}
+  
 
 function checkWin() {
     for (let i = 0; i < winConditions.length; i++) {
