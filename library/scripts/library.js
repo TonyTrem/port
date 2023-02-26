9ce4253cb054b35ab4efb00e2205468320816668
TonyTrem <117864473+TonyTrem@users.noreply.github.com>
2023-02-27 00:17:43 +1300

    Try library differently

--- library/scripts/library.js
+++ library/scripts/library.js
@@ -1,39 +1,87 @@
-let myLibrary = [];
+const bookList = document.getElementById("bookList");
+let books = getBooksFromStorage();
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function() {
-        return `${title} by ${author}, ${pages} pages, ${read}`;
-    }   
-}
+function addBook() {
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+  const pages = document.getElementById("pages").value;
+  const read = document.getElementById("read").checked; 
 
-function addBookToLibrary() {
-    let title = prompt('Enter the title of the book');
-    let author = prompt('Enter the author of the book');
-    let pages = prompt('Enter the number of pages');
-    let read = prompt('Have you read the book?');
-    let newBook = new Book(title, author, pages, read);
-    myLibrary.push(newBook);
+  const book = {
+    title,
+    author,
+    pages: parseInt(pages),
+    read
+  };
+
+  books.push(book);
+
+  displayBooks();
+  saveBooksToStorage();
 }
 
 function displayBooks() {
-    let library = document.querySelector('#library');
-    library.innerHTML = '';
-    for (let i = 0; i < myLibrary.length; i++) {
-        let book = document.createElement('div');
-        book.classList.add('book');
-        book.innerHTML = myLibrary[i].info();
-        library.appendChild(book);
-    }
+  bookList.innerHTML = "";
+
+  books.forEach(function(book, index) {
+    const li = document.createElement("li");
+  
+    const titleDiv = document.createElement("div");
+    titleDiv.classList.add("title");
+    titleDiv.textContent = book.title;
+    li.appendChild(titleDiv);
+
+    const authorDiv = document.createElement("div");
+    authorDiv.classList.add("author");
+    authorDiv.textContent = book.author;
+    li.appendChild(authorDiv);
+
+    const pagesDiv = document.createElement("div");
+    pagesDiv.classList.add("pages");
+    pagesDiv.textContent = book.pages;
+    li.appendChild(pagesDiv);
+
+    const readDiv = document.createElement("div");
+    readDiv.classList.add("read");
+    readDiv.textContent = book.read ? "Read" : "Not read";
+    li.appendChild(readDiv);
+  
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    deleteButton.addEventListener("click", function() {
+      books.splice(index, 1);
+      displayBooks();
+      saveBooksToStorage();
+    });
+    li.appendChild(deleteButton);
+
+    bookList.appendChild(li);
+  });
 }
 
+function sortBooks() {
+  books.sort(function(a, b) {
+  return a.title.localeCompare(b.title);
+  });
 
+  displayBooks();
+}
 
-let theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, 'not read yet');
-let theLordOfTheRings = new Book('The Lord of the Rings', 'J.R.R. Tolkien', 1216, 'read');
+function deleteAllBooks() {
+  if (confirm("Are you sure you want to delete all books?")) {
+  books = [];
+  displayBooks();
+  saveBooksToStorage();
+  }
+}
 
-console.log(theHobbit.info());
-console.log(theLordOfTheRings.info());
+function getBooksFromStorage() {
+  const booksString = localStorage.getItem("books");
+  return booksString ? JSON.parse(booksString) : [];
+}
+
+function saveBooksToStorage() {
+  localStorage.setItem("books", JSON.stringify(books));
+}
+
+displayBooks();
