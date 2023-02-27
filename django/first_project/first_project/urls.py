991b7521349284da64b4f883020bf8a24fe76512
TonyTrem <117864473+TonyTrem@users.noreply.github.com>
2023-02-27 15:52:09 +1300

    Complete new year app

--- django/first_project/first_project/urls.py
+++ django/first_project/first_project/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('myfirstapp/', include('myfirstapp.urls')),
+    path('newyear/', include('newyear.urls')),
 ]
